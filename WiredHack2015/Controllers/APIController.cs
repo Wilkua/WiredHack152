2770a07c686c879bb794730630814e991319770b
William Drescher <wilkua2@hotmail.com>
2015-12-09 00:14:30 -0500

    Create /API/CitiesFromState REST call

--- WiredHack2015/Controllers/APIController.cs
+++ WiredHack2015/Controllers/APIController.cs
@@ -2,16 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Serialization;
 using System.Web.Mvc;
 
 namespace WiredHack2015.Controllers
 {
     public class APIController : Controller
     {
+        private WiredHackEntities dbContext = new WiredHackEntities();
+
         // GET: API
-        public ActionResult Index()
+        public string Index()
         {
-            return View();
+            return "<!DOCTYPE html><html lang=\"en\"><head><title></title><meta charset=\"utf-8\" /></head><body></body></html>";
+        }
+
+        // GET: API/HeatmapData?
+        public string HeatmapData()
+        {
+            return "";
+        }
+
+        // GET: API/CitiesFromState?state=XX
+        public string CitiesFromState(string state)
+        {
+            // The state should only be 2 letters
+            if ((!String.IsNullOrEmpty(state)) && (state.Length > 2))
+            {
+                return "";
+            }
+
+            List<String> cities;
+
+            if (String.IsNullOrEmpty(state))
+            {
+                cities = dbContext.stgDealers.Select(s => s.City).Distinct().ToList();
+            }
+            else
+            {
+                cities = dbContext.stgDealers.Where(s => s.State == state).Select(s => s.City).Distinct().ToList();
+            }
+
+            var JSONSerializer = new JavaScriptSerializer();
+
+            return JSONSerializer.Serialize(cities);
         }
     }
 }
